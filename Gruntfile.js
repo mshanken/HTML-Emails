f30b7bac2b35c46b6085ba20cb24e1c70dcad9d1
Edison Leon <eleon@mshanken.com>
2018-06-20 14:16:48 -0400

    updating to bootstrap 4.1

--- Gruntfile.js
+++ Gruntfile.js
@@ -54,8 +54,8 @@ module.exports = function(grunt) {
     'copy:ghpages'
   ]);
   grunt.registerTask('start', [
-    'bower',
-    'frep',
+    'bower'
+    // 'frep',
   ]);
   grunt.registerTask('lint', [
     'jscs',
